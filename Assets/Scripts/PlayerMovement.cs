33daddb1f0ad16c19cf2ae25cdc0f7dd43389b81
DavidCM <dcm.robin@gmail.com>
2023-01-06 19:21:22 +0000

    wall jumping!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 6f; // movement speed
     public float jumpForce = 10f; // force applied when jumping
     public float gravity = 20f; // gravity applied to the player
+    public bool canWallJump = true; // whether the player can wall jump
 
     private Vector3 moveDirection = Vector3.zero; // current movement direction
     private CharacterController controller; // reference to the character controller component
+    private float wallJumpTimeout = 0f; // timeout for wall jumping
 
     void Start()
     {
@@ -35,6 +37,31 @@ public class PlayerMovement : MonoBehaviour
             {
                 moveDirection.y = jumpForce;
             }
+        }
+        // if the character controller is not grounded
+        else
+        {
+            // check if the player can wall jump and has pressed the jump button
+            if (canWallJump && Input.GetButtonDown("Jump") && wallJumpTimeout <= 0)
+            {
+                // get input for the horizontal axis
+                float h = Input.GetAxis("Horizontal");
+
+                // check if the player is moving towards a wall
+                if (h != 0)
+                {
+                    // calculate the wall jump direction
+                    moveDirection = transform.right * h;
+                    moveDirection = moveDirection.normalized * speed;
+                    moveDirection.y = jumpForce;
+
+                    // set the wall jump timeout
+                    wallJumpTimeout = 0.5f;
+                }
+            }
+
+            // decrease the wall jump timeout
+            wallJumpTimeout -= Time.deltaTime;
         }
 
         // apply gravity to the character controller
